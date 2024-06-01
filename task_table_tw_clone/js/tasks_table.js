6d0757b3c37cae19474a4e9f50d0838b1f4ff312
jhickmanarc <jhickman@arcdigitalservices.com>
2024-05-31 17:30:28 -0700

    Improved documentation

--- task_table_tw_clone/js/tasks_table.js
+++ task_table_tw_clone/js/tasks_table.js
@@ -65,7 +65,7 @@ class TaskTable {
      * Find the data object by row UUID
      * 
      * @param {string} uid 
-     * @returns Array that contans the row data, index in the array
+     * @returns Array that contans [row data, index in the array]
      */
     find_by_uid(uid) {
         const scan = (d) => {
